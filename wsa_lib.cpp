bd7b7d9ded1a3450eb2b3a0fda0a664fc186a736
QSy <quyen@thinkrf.com>
2011-10-24 13:07:40 -0400

    Swapped I & Q order due to the 440 hardware... (I think)

--- wsa_lib.cpp
+++ wsa_lib.cpp
@@ -703,15 +703,16 @@ int32_t wsa_decode_frame(char *data_buf, int16_t *i_buf, int16_t *q_buf,
 	// *****
 	for (i = 0; i < sample_size * 4; i += 4) {
 		// Gets the payload, each word = I2I1Q2Q1 bytes
-		i_buf[j] = (((uint8_t) data_buf[i]) << 8) + ((uint8_t) data_buf[i + 1]); 
-		q_buf[j] = (((uint8_t) data_buf[i + 2]) << 8) + (uint8_t) data_buf[i + 3];
+		// Noticed IQ order swapped due to 440 hardware
+		q_buf[j] = (((uint8_t) data_buf[i]) << 8) + ((uint8_t) data_buf[i + 1]); 
+		i_buf[j] = (((uint8_t) data_buf[i + 2]) << 8) + (uint8_t) data_buf[i + 3];
 
-
+		// Do the signed 2-complement on the 14-bit sample
 		if (i_buf[j] > 8191) i_buf[j] -= 16384;
 		if (q_buf[j] > 8191) q_buf[j] -= 16384;
 		
-		//if ((j % 4) == 0) printf("\n");
-		//printf("%04x,%04x ", i_buf[j], q_buf[j]);
+		/*if ((j % 4) == 0) printf("\n");
+		printf("%04x,%04x ", i_buf[j], q_buf[j]);*/
 		
 		j++;
 	}
