8fa2140c3cff212dc88173436c2ed9c7a39c8950
marek.menhart <marek.menhart@thinkrf.com>
2012-04-05 16:17:26 -0400

    Add wsa_read_iq_packet to wsa_api.h header

--- api/include/wsa_api.h
+++ api/include/wsa_api.h
@@ -39,6 +39,12 @@ int32_t wsa_frame_decode(struct wsa_device *dev, uint8_t* data_buf, int16_t *i_b
 int32_t wsa_read_frame_int(struct wsa_device *dev, struct wsa_frame_header 
 		*header, int16_t *i_buf, int16_t *q_buf, const int32_t sample_size);
 
+int16_t wsa_read_iq_packet (struct wsa_device* const device, 
+		struct wsa_frame_header* const header, 
+		int16_t* const i_buffer, 
+		int16_t* const q_buffer,
+		const uint16_t samples_per_packet);
+
 int16_t wsa_get_sample_size(struct wsa_device *dev, int32_t *sample_size);
 int16_t wsa_set_sample_size(struct wsa_device *dev, int32_t sample_size);
 
