0f4fb200406c363b9c40d661fa36308f0077d8cb
Mohammad Farhan <mohammad.farhan@thinkrf.com>
2014-03-27 13:05:14 -0400

    added shn rfe mode

--- api/src/wsa_api.c
+++ api/src/wsa_api.c
@@ -1123,6 +1123,7 @@ int16_t wsa_get_rfe_input_mode(struct wsa_device *dev, char *mode)
 	if (strcmp(mode, WSA_RFE_ZIF_STRING) != 0 &&
 		strcmp(mode, WSA_RFE_HDR_STRING) != 0 &&
 		strcmp(mode, WSA_RFE_SH_STRING) != 0 &&
+		strcmp(mode, WSA_RFE_SHN_STRING) != 0 &&
 		strcmp(mode, WSA_RFE_IQIN_STRING) != 0)
 		return WSA_ERR_INVRFEINPUTMODE;
 
@@ -1150,6 +1151,7 @@ int16_t wsa_set_rfe_input_mode(struct wsa_device *dev, char *mode)
 	if (strcmp(mode, WSA_RFE_ZIF_STRING) != 0 &&
 		strcmp(mode, WSA_RFE_HDR_STRING) != 0 &&
 		strcmp(mode, WSA_RFE_SH_STRING) != 0 &&
+		strcmp(mode, WSA_RFE_SHN_STRING) != 0 &&
 		strcmp(mode, WSA_RFE_IQIN_STRING) != 0)
 		return WSA_ERR_INVRFEINPUTMODE;
 
@@ -2170,6 +2172,7 @@ int16_t wsa_get_sweep_rfe_input_mode(struct wsa_device *dev, char *mode)
 	if (strcmp(mode, WSA_RFE_ZIF_STRING) != 0 &&
 		strcmp(mode, WSA_RFE_HDR_STRING) != 0 &&
 		strcmp(mode, WSA_RFE_SH_STRING) != 0 &&
+		strcmp(mode, WSA_RFE_SHN_STRING) != 0 &&
 		strcmp(mode, WSA_RFE_IQIN_STRING) != 0)
 		return WSA_ERR_INVRFEINPUTMODE;
 
@@ -2197,6 +2200,7 @@ int16_t wsa_set_sweep_rfe_input_mode(struct wsa_device *dev, char *mode)
 	if (strcmp(mode, WSA_RFE_ZIF_STRING) != 0 &&
 		strcmp(mode, WSA_RFE_HDR_STRING) != 0 &&
 		strcmp(mode, WSA_RFE_SH_STRING) != 0 &&
+		strcmp(mode, WSA_RFE_SHN_STRING) != 0 &&
 		strcmp(mode, WSA_RFE_IQIN_STRING) != 0)
 		return WSA_ERR_INVRFEINPUTMODE;
 
