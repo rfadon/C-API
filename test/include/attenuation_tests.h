19762ece8931aa055cbfe4ea93813cb896b77073
Mohammad Farhan <mohammad.farhan@thinkrf.com>
2017-05-18 11:20:05 -0400

    began implementing a test util file

--- test/include/attenuation_tests.h
+++ test/include/attenuation_tests.h
@@ -1,10 +1,7 @@
 
-#include <stdio.h>
-#include <string.h>
-#include <ctype.h>
-#include <wsa_api.h>
-#include <wsa_lib.h>
-#include <wsa_sweep_device.h>
-#include <wsa_error.h>
 
-int16_t attenuation_tests(struct wsa_device *dev, int32_t *fail_count, int32_t *pass_count);
+#include <test_util.h>
+
+
+
+int16_t attenuation_tests(struct wsa_device *dev, struct test_data *test_info);
